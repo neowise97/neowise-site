23e8e59bc991379f4d081da832addf92fdfa093e
neowise97 <brucpetro@gmail.com>
2026-07-25 16:19:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,11 @@
 const menu = document.querySelector(".menu-toggle");
 const nav = document.querySelector("nav ul");
 
-menu.addEventListener("click", () => {
-    nav.classList.toggle("active");
-});
+if (menu && nav) {
+    menu.addEventListener("click", () => {
+        nav.classList.toggle("active");
+    });
+}
 
 // Закривати меню після натискання
 
